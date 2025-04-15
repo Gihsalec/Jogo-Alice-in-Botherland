5e1c36e93252654a306b24c50a2a71a500ab5a89
Giovanna Sales <giovannasalesgcs@gmail.com>
2025-04-15 10:43:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+function next1() {
+    escolhajog = 1
+    escolharei = Math.floor(Math.random() * 3) + 1;
+    if (escolhajog == escolharei){
+            window.location.href = "../menoshp/menoshp.html"; 
+        }
+    
+    if (escolhajog != escolharei){
+        window.location.href = "../escapou/esc.html";
+    }
+
+}
+
+function next2() {
+    escolhajog = 2
+    escolharei = Math.floor(Math.random() * 3) + 1;
+    if (escolhajog == escolharei){ 
+            window.location.href = "../menoshp/menoshp.html"; 
+        }
+    
+    if (escolhajog != escolharei){
+        window.location.href = "../escapou/esc.html";
+    }
+}
+
+function next3() {
+    escolhajog = 3
+    escolharei = Math.floor(Math.random() * 3) + 1;
+    if (escolhajog == escolharei){
+        window.location.href = "../menoshp/menoshp.html"; 
+        }
+    
+    if (escolhajog != escolharei){
+        window.location.href = "../escapou/esc.html";
+    }
+}
+
+
+    
+ 
+
+
+   
